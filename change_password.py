fc7c2a845ae2c55bab4b06ea1df9deb778204abf
jedrekw <jedrekw@op.pl>
2016-11-15 10:27:40 +0100

    2

--- change_password.py
+++ change_password.py
@@ -3,13 +3,13 @@ from utils.utils import *
 def main():
     _saved_password1 = get_password()
 
-def get_password(filename='change_pass.txt'):
+def get_password(filename='change_pass1.txt'):
         fin = open(filename, 'r')
         old_password = fin.readline()[:-1]
         fin.close()
         return old_password
 
-def change_password_value(filename='change_pass.txt'):
+def change_password_value(filename='change_pass1.txt'):
         fout = open(filename, 'w')
         _new_password = get_random_uuid(9)
         fout.write(_new_password+'\n')
