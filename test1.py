37697f92706393f8fa3df01af488a60a47fc3a53
jedrekw <jedrekw@op.pl>
2017-11-19 15:22:06 +0100

    popr 19-11

--- test1.py
+++ test1.py
@@ -844,7 +844,7 @@ class SmokeTest(unittest.TestCase):
             opts.add_argument("--no-default-browser-check") #Overrides default choices
             opts.add_argument("--no-first-run")
             opts.add_argument("--disable-default-apps")
-            self.driver = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver", service_args=sr_args, chrome_options=opts)
+            self.driver = webdriver.Chrome(executable_path="/var/lib/jenkins/workspace/Testarmy/node_modules/chromedriver", service_args=sr_args, chrome_options=opts)
             # self.driver = webdriver.Chrome(service_args=sr_args, chrome_options=opts)
             self.driver.set_window_size(1024,768)
             # self.driver.implicitly_wait(self.timeout)
